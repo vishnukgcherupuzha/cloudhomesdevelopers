9a9f887ae1a64542c153064362b788c550518415
vishnukgcherupuzha <vishnukgcherupuzha@gmail.com>
2019-10-11 14:58:16 +0530

    commit

--- cloudhomesdevelopers/src/components/Contactus.js
+++ cloudhomesdevelopers/src/components/Contactus.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import Contact from '../Images/contact-us.png'
+import ContactHead from '../Images/contactus.jpg'
 import axios from 'axios';
 import ReactSnackBar from "react-js-snackbar";
+import {Table,tbody} from 'react-bootstrap'
 class Contactus extends Component {
     constructor(props) {
         super(props)
@@ -50,52 +51,25 @@ class Contactus extends Component {
             <div>
                 {this.notifyActionStatus()}
                 <div>
-                    <div className="map mb-4 mb-lg-0" >
-                        <iframe
-                            className="embed-responsive-item"
-                            title="Maps"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387.3616825122146!2d75.37073766998817!3d11.87984952275257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDUyJzQ3LjQiTiA3NcKwMjInMTUuMSJF!5e0!3m2!1sen!2sin!4v1568007241938!5m2!1sen!2sin"
-                            frameBorder="0" style={{ border: '0', width: '100%', height: '500px' }}
-                            display="initial"
-                            position="relative"
-                            allowFullScreen>
-                        </iframe>
-                    </div>
+                <img src={ContactHead} className="img-fluid" alt="" data-aos="zoom-out"></img>
                     <section id="contact">
                         <div className="container-fluid">
                             <div className="container">
                                 <div className="section-header" data-aos="zoom-out">
                                     <h3 style={{ color: '#f0f0f0' }}>Contact Us</h3>
-                                </div>
-                                <div className="row wow fadeInUp" >
-                                    <div className="col-lg-12" data-aos="zoom-out">
-                                        <div className="row" style={{ padding: '10px 0' }}>
-                                            <div className="col-md-5 info">
-                                                <i className="fa fa-map-marker"></i>
-                                                <p style={{ color: '#f0f0f0' }}>Cloud Homes Developers, 4/121 Opp.Kalyan Sarees, Near Stadium Complex, Kannur </p>
-                                            </div>
-                                            <div className="col-md-4 info">
-                                                <i className="fa fa-envelope"></i>
-                                                <p style={{ color: '#f0f0f0' }}>info@cloudhomesdevelopers.com</p>
-                                            </div>
-                                            <div className="col-md-3 info">
-                                                <i className="fa fa-phone"></i>
-                                                <p style={{ color: '#f0f0f0' }}>+91-9747867892</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <p>We'd love to hear from you.
+                                    Feel free to contact us with your requirements and our representatives will get in touch with you.</p>
                                 </div>
                             </div>
                         </div>
+                        
                         <div className="container">
                             <div className="row align-items-lg-center">
                                 <div className="form col-12 col-md-6 text-center text-md-left" data-aos="zoom-out" >
-                                    <div id="sendmessage">Your message has been sent. Thank you!</div>
-                                    <div id="errormessage"></div>
+                                    <section style={{paddingTop:"0"}}>
                                     <form
                                         onSubmit={this.handleSubmit}
-                                        className="contactForm"
-                                    >
+                                        className="contactForm">
                                         <div className="form-row">
                                             <div className="form-group col-lg-6">
                                                 <input
@@ -149,13 +123,41 @@ class Contactus extends Component {
                                         </div>
                                         <div className="text-center"><button type="submit" title="Send Message">Send Message</button></div>
                                     </form>
+                                    </section>
                                 </div>
-                                <div className="col-12 col-md-6" data-aos="zoom-out">
-                                    <img src={Contact} className="img-fluid" alt=""></img>
+                                <div className="col-12 col-md-6" style={{paddingBottom:"60px"}} data-aos="zoom-out">
+                                    <Table striped borderless variant="dark" hover style={{borderRadius:"20px"}}>
+                                        <tbody>
+
+                                            <tr className="col-md-12 info">
+                                                <td><i className="fa fa-map-marker"></i></td>
+                                                <td> <p style={{ color: '#f0f0f0' }}>Cloud Homes Developers, 4/121 Opp.Kalyan Sarees, Near Stadium Complex, Kannur </p></td>
+                                            </tr>
+                                            <tr className="col-md-12 info">
+                                                    <td><i className="fa fa-envelope"></i></td>
+                                                    <td><p style={{ color: '#f0f0f0' }}>info@cloudhomesdevelopers.com</p></td>
+                                            </tr>
+                                            <tr className="col-md-12 info">
+                                                    <td><i className="fa fa-phone"></i></td>
+                                                    <td><p style={{ color: '#f0f0f0' }}>+91-9747867892</p></td>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
                                 </div>
                             </div>
                         </div>
                     </section>
+                    <div className="map mb-4 mb-lg-0" >
+                        <iframe
+                            className="embed-responsive-item"
+                            title="Maps"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387.3616825122146!2d75.37073766998817!3d11.87984952275257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDUyJzQ3LjQiTiA3NcKwMjInMTUuMSJF!5e0!3m2!1sen!2sin!4v1568007241938!5m2!1sen!2sin"
+                            frameBorder="0" style={{ border: '0', width: '100%', height: '500px' }}
+                            display="initial"
+                            position="relative"
+                            allowFullScreen>
+                        </iframe>
+                    </div>
                 </div>
             </div>
         )
